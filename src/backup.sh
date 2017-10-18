689066cf21721ecdc8aac4a0060e1853f15eddf7
nik <gavalnick@gmail.com>
2017-10-18 11:46:26 +0000

    Update 'src/backup.sh'

--- src/backup.sh
+++ src/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mysqldump -u root -p"yourpasswordhere" --all-databases > /path/of/your/choice
-backup_files="path/to/gitea/ /path/of/your/choice /etc/letsencrypt /etc/apache2 /path/to/pihole /var/www/nextcloud /var/www/othersites"
+backup_files="path/to/gitea/ /path/of/your/choice /etc/letsencrypt /etc/nginx /path/to/pihole /var/www/nextcloud /var/www/othersites"
 
 dest="/media/your_disk/backup"
 
