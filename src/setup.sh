b00aecb29a525a9612b11e83073bc9acfc04c1d0
nik <gavalnick@gmail.com>
2017-10-19 10:47:18 +0000

    Add 'src/setup.sh'

---
+++ src/setup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+YES=
+if [ "$1" = "-y" ]; then
+        shift
+        YES=1
+fi
+
+STEP () {
+        if [ -z "$YES" ]; then
+        read -p 'Do it? (Y/n) ' c
+        [ -z "$c" ] || [ "$c" = "Y" ] || [ "$c" = "y" ]
+        else
+        true
+        fi
+}
+
+set -v
+cd ${HOME}
+
+STEP Install git, vim, and other tools && {
+        apt-get install git vim bash-completion bc htop
+}
+
+STEP Enable greek UTF-8 locale && {
+        sed -i -e 's/^# *\(el_GR.UTF-8\)$/\1/g' /etc/locale.gen
+        locale-gen
+}
+
+STEP Enable greek UTF-8 typing in shell && {
+        touch ~/.bashrc
+        sed -i -e '/^export LC_CTYPE=/d' ~/.bashrc
+        echo 'export LC_CTYPE=el_GR.UTF-8' '>>' ~/.bashrc
+}
+
+
+STEP Download and install bashrc and vimrc && {
+        git clone https://code.nickgavalas.com/nik/LinuxScripts.git
+        mv ~/.bashrc ~/.bashrc.default
+        mv ~/.vimrc ~/.vimrc.default
+        cp ./LinuxScripts/src/bashrc ~/.bashrc
+        cp ./LinuxScripts/src/vimrc ~/.vimrc
+}
+
+echo 'Done.'
