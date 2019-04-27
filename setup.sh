3724d829b053bcc126c10b43f06f71b8b4b3ac1f
Nick Gavalas <gavalnick@gmail.com>
2019-04-27 21:16:38 +0300

    Silence stderr in setup.sh

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ fi
 
 for file in "${FILES[@]}"
 do
-    mv "$HOME/${file}" "$HOME/${file}.old" || echo "created ${file}"
+    mv "$HOME/${file}" "$HOME/${file}.old" 2> /dev/null || echo "created ${file}"
     curl -fsSL -o $HOME/${file} "https://raw.githubusercontent.com/NikosGavalas/dotfiles/master/${file}"
 done
 
