3934677f107737192646eb9c97664986eff6528c
Nikos Gavalas <gavalnick@gmail.com>
2019-07-11 11:37:19 +0200

    Minor fix

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ FILES=(
 echo "backing up old files and fetching the new ones..."
 for file in "${FILES[@]}"; do
     mv "$HOME/${file}" "$HOME/${file}.old" 2>/dev/null || echo "creating ${file}..."
-    curl -fsSL -o "$HOME/${file}" "https://raw.githubusercontent.com/NikosGavalas/dotfiles/master/${file}" && "created ${file}"
+    curl -fsSL -o "$HOME/${file}" "https://raw.githubusercontent.com/NikosGavalas/dotfiles/master/${file}" && echo "created ${file}"
 done
 
 echo "Done. You can reset configuration files by running \"rollback_config\""
