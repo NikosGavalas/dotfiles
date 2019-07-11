047dc96419b227ae4639e443efe84e5cf42037f5
Nikos Gavalas <gavalnick@gmail.com>
2019-07-11 11:55:23 +0200

    Fix some stuff in setup.sh

--- setup.sh
+++ setup.sh
@@ -11,10 +11,13 @@ FILES=(
     ".config/Code/User/settings.json"
 )
 
+mkdir -p "$HOME/.config/nvim"
+mkdir -p "$HOME/.config/Code/User"
+
 echo "backing up old files and fetching the new ones..."
 for file in "${FILES[@]}"; do
     mv "$HOME/${file}" "$HOME/${file}.old" 2>/dev/null || echo "creating ${file}..."
-    curl -fsSL -o "$HOME/${file}" "https://raw.githubusercontent.com/NikosGavalas/dotfiles/master/${file}" && echo "created ${file}"
+    curl -fsSL -o "$HOME/${file}" "https://raw.githubusercontent.com/NikosGavalas/dotfiles/master/${file}"
 done
 
 echo "Done. You can reset configuration files by running \"rollback_config\""
