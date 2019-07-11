c0801b00e4663e5074eec68032855e4ffe422a76
Nikos Gavalas <gavalnick@gmail.com>
2019-07-11 11:36:44 +0200

    Minor edit

--- setup.sh
+++ setup.sh
@@ -11,8 +11,10 @@ FILES=(
     ".config/Code/User/settings.json"
 )
 
-echo "backing up old files..."
+echo "backing up old files and fetching the new ones..."
 for file in "${FILES[@]}"; do
-    mv "$HOME/${file}" "$HOME/${file}.old" 2> /dev/null || echo "created ${file}"
-    curl -fsSL -o $HOME/${file} "https://raw.githubusercontent.com/NikosGavalas/dotfiles/master/${file}"
+    mv "$HOME/${file}" "$HOME/${file}.old" 2>/dev/null || echo "creating ${file}..."
+    curl -fsSL -o "$HOME/${file}" "https://raw.githubusercontent.com/NikosGavalas/dotfiles/master/${file}" && "created ${file}"
 done
+
+echo "Done. You can reset configuration files by running \"rollback_config\""
