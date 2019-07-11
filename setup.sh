3985a771aee9e9bb9f9d199400933ae6bcea6fd9
Nikos Gavalas <gavalnick@gmail.com>
2019-07-11 11:16:00 +0200

    Add zshrc, moved function etc. to commonrc for bash compatibility, add tmux conf, nvim conf and moved vscode settings

--- setup.sh
+++ setup.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-FILES=(".bashrc" ".vimrc" ".gitconfig")
+FILES=(
+    ".bashrc"
+    ".zshrc"
+    ".commonrc"
+    ".vimrc"
+    ".gitconfig"
+    ".tmux.conf"
+    ".config/nvim/init.vim"
+    ".config/Code/User/settings.json"
+)
 
+echo "backing up old files..."
 for file in "${FILES[@]}"; do
     mv "$HOME/${file}" "$HOME/${file}.old" 2> /dev/null || echo "created ${file}"
     curl -fsSL -o $HOME/${file} "https://raw.githubusercontent.com/NikosGavalas/dotfiles/master/${file}"
 done
-
