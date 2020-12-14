e0bd93b6b025bb52a80548ffd419fcd52badd5b5
Nick <gavalnick@gmail.com>
2020-12-14 13:13:19 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,18 +12,20 @@ FILES=(
     ".zshrc"
 )
 
+touch "$HOME/.custom"
+
 echo "backing up old files and fetching the new ones..."
 for file in "${FILES[@]}"; do
     mv "$HOME/${file}" "$HOME/${file}.old" 2>/dev/null || echo "creating ${file}..."
     curl -fsSL -o "$HOME/${file}" "https://raw.githubusercontent.com/NikosGavalas/dotfiles/master/src/${file}"
 done
 
-rollback_config() {
+rollback-config() {
     for file in "${FILES[@]}"; do
         mv "$HOME/${file}.old" "$HOME/${file}" 2>/dev/null || (rm "$HOME/${file}" && echo "removed ${file}")
     done
 }
 
-export -f rollback_config
+export -f rollback-config
 
-echo "Done. You can reset configuration files by running \"rollback_config\""
+echo "Done. You can reset configuration files by running \"rollback-config\""
