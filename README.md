930356cd38fa794693e3e15e5337b14d0a5d2e67
Nikos Gavalas <gavalnick@gmail.com>
2021-05-28 12:52:33 +0300

    add shortcut script, add my git creds into gitconfig

--- README.md
+++ README.md
@@ -10,4 +10,6 @@ Update with:
 update-config
 ```
 
+**Warning**: I wrote these for my personal use. If you want to use them yourself, audit them first, as they contain settings you probably don't want.
+
 Some functions in .bashrc and git aliases by [G. Mantaos](https://gmantaos.com)
--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 FILES=(
     ".aliases"
     ".bash_completion"
@@ -14,6 +15,14 @@ FILES=(
 
 touch "$HOME/.custom"
 
+mkdir -p "$HOME/.shortcuts"
+mkdir -p "$HOME/.local/bin"
+cat << EOF > "$HOME/.local/bin/shortcut"
+#!/bin/bash -i
+[[ -z "\$1" ]] && echo "usage: \$0 <shortcut-name>" >&2 && exit 1
+bash -i "$HOME/.shortcuts/\$1"
+EOF
+
 echo "backing up old files and fetching the new ones..."
 for file in "${FILES[@]}"; do
     mv "$HOME/${file}" "$HOME/${file}.old" 2>/dev/null || echo "creating ${file}..."
@@ -25,7 +34,6 @@ rollback-config() {
         mv "$HOME/${file}.old" "$HOME/${file}" 2>/dev/null || (rm "$HOME/${file}" && echo "removed ${file}")
     done
 }
-
 export -f rollback-config
 
 echo "Done. You can reset configuration files by running \"rollback-config\""
